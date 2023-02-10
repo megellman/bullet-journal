287140265da7c66b76715c42fe518549f7da2b69
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-10 15:56:37 -0800

    write main.handlebars

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/bulma', express.static(__dirname + '/node_modules/bulma/css/'));
 
 app.use(routes);
 
