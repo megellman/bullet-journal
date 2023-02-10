089f3178f5b1d5b9020fb134c4a4ca830a8d67c0
captaiiinsolo <solo.d.santos@gmail.com>
2023-02-10 12:47:54 -0800

    add response for whene entry is successfully deleted

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -31,6 +31,8 @@ router.delete("/:id", withAuth, async (req, res) => {
             res.status(404).json({ message: "No entry found with this ID!"});
             return;
         }
+
+        res.status(200).json(entryData);
     } catch (err) {
         res.status(500).json(err);
     }
