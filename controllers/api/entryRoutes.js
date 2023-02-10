20e1ddab4e9f0f49c13885cb2f0f795c812a85ad
captaiiinsolo <solo.d.santos@gmail.com>
2023-02-10 13:28:46 -0800

    create update route for entries

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -17,6 +17,27 @@ router.post("/", withAuth, async (req, res) => {
     }
 });
 
+
+// Update entry
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const entryData = await Entry.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!entryData) {
+            res.status(404).json({ message: "No entry found with this ID!"});
+            return;
+        }
+
+        res.status(200).json(entryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // Delete Specific Entry
 router.delete("/:id", withAuth, async (req, res) => {
     try {
