16b2302c2a30b38f48c2b55ba47570bf17050fe7
Megan Ellman <meganlellman@gmail.com>
2023-02-10 10:45:38 -0800

    created journal seed data

---
+++ seeds/journalSeed.js
@@ -0,0 +1,32 @@
+[
+    {
+        "title": "Ideas",
+        "date": 2016-10-02,
+        "description": "Jotting down anything that inspires me",
+        "user_id": 1,
+    },
+    {
+        "title": "Personal",
+        "date": 2010-04-19,
+        "description": "Remembering all the details that make up my life",
+        "user_id": 2,
+    },
+    {
+        "title": "Travel",
+        "date": 2020-03-11,
+        "description": "Memories from all over the world",
+        "user_id": 3,
+    },
+    {
+        "title": "Creative",
+        "date": 2019-05-22,
+        "description": "Short stories, songs, poems and such",
+        "user_id": 4,
+    },
+    {
+        "title": "Feelings",
+        "date": 2017-09-15,
+        "description": "A place to remember and reflect",
+        "user_id": 5,
+    },
+]
