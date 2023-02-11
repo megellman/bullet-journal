5d5b9f3c795154a7c628d5a8dfd9e3ea131c23f3
Megan Ellman <meganlellman@gmail.com>
2023-02-11 14:34:53 -0800

    fixed auth

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User } = require("../../models");
-const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
     try {
@@ -22,7 +21,7 @@ router.post('/', async (req, res) => {
       }
 });
 
-router.post('/login', withAuth, async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
     if (!userData) {
