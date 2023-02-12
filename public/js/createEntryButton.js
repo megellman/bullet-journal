41437f233a29990da85ebab2d579610bb333a3b9
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-11 18:27:53 -0800

    start writing JavaScript for Journal page

---
+++ public/js/createEntryButton.js
@@ -0,0 +1,9 @@
+const createButton = document.getElementById("create-entry-button");
+
+async function handleCreateEntryButton (event) {
+    event.preventDefault();
+
+    // document.location.replace("")
+}
+
+createButton.addEventListener("click", handleCreateEntryButton);
